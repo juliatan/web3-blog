adcff8ae2005411eceb153b07473420b3a5887ea
Julia Tan <juliajtan@gmail.com>
2022-02-15 21:29:04 +0000

    Deploy smart contract

--- README.md
+++ README.md
@@ -1,24 +1,18 @@
 ## Running a blog on the blockchain
 
+- Practice project where I'm following along to a tutorial.
+
 ## Getting Started
 
-First, run the development server:
-
-```bash
-npm run dev
-# or
-yarn dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-To run the tests:
-`npx hardhat test`
+- Run the development server: `npm run dev` or `yarn dev`. Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+- To run the tests: `npx hardhat test`.
+- To see test accounts: `npx hardhat node`. This command also starts the local network (so ensure this is running when you deploy your contract).
 
 ## How it was built
 
-- Install NextJS and packages
-- Amend `hardhat.config.js`
-- Set up global styles and add relevant images to `public` folder
-- Add Blog contract to `contracts` folder
-- Set up blog tests in `test` folder
+- Install NextJS and packages.
+- Amend `hardhat.config.js`.
+- Set up global styles and add relevant images to `public` folder.
+- Add Blog contract to `contracts` folder.
+- Set up blog tests in `test` folder to check you're happy with the contract.
+- Set up the `deploy.js` script to deploy the contract. Ensure the local network is running using `npx hardhat node`, then deploy using `npx hardhat run scripts/deploy.js --network localhost`.
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,8 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+const hre = require('hardhat')
+const fs = require('fs')
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -13,13 +14,22 @@ async function main() {
   // manually to make sure everything is compiled
   // await hre.run('compile');
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  /* these two lines deploy the contract to the network */
+  const Blog = await hre.ethers.getContractFactory('Blog')
+  const blog = await Blog.deploy('My blog')
 
-  await greeter.deployed();
+  await blog.deployed()
+  console.log('Blog deployed to:', blog.address)
 
-  console.log("Greeter deployed to:", greeter.address);
+  /* this code writes the contract addresses to a local */
+  /* file named config.js that we can use in the app */
+  fs.writeFileSync(
+    './config.js',
+    `
+  export const contractAddress = "${blog.address}"
+  export const ownerAddress = "${blog.signer.address}"
+  `,
+  )
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -27,6 +37,6 @@ async function main() {
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    console.error(error)
+    process.exit(1)
+  })
