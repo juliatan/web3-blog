6e669114735930ee2970e9a108cb4fe86178e7a5
Julia Tan <juliajtan@gmail.com>
2022-02-15 20:43:09 +0000

    Install hardhat and finish setup

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+require('@nomiclabs/hardhat-waffle')
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: '0.8.4',
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    // mumbai: {
+    //   url: "https://rpc-mumbai.matic.today",
+    //   accounts: [process.env.pk]
+    // },
+    // polygon: {
+    //   url: "https://polygon-rpc.com/",
+    //   accounts: [process.env.pk]
+    // }
+  },
+}
